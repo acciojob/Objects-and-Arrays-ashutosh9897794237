d8fb7b9293aece93bade3fe6321741bcd11fea5f
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-03-24 13:32:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const person = {
+      name: 'Alice',
+      age: 75
+    };
 
+const team1 = ['Bob', 'Poppy'];
+
+const person = {
+      name: 'Bob',
+      age: 70
+    };
+
+// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const person = {
+      name: 'Poppy',
+      age: 65
+    };
+
+const cap1 = {
+      name: 'Poppy',
+      age: 65
+    };
 
 export {players,person,team,team1,cap1}
